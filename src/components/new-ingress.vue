b4ba699edd499682ac1b8351eb314a5bfb298fcf
lucklove <gnu.crazier@gmail.com>
2017-04-10 14:12:25 +0800

    Use select instead of input

--- src/components/new-ingress.vue
+++ src/components/new-ingress.vue
@@ -26,7 +26,7 @@
                         </div>
                     </div>
                     <div class="form-group form-md-line-input has-success">
-                        <input type="text" class="form-control" placeholder="Host" v-model="ingress.host">
+                        <input type="text" class="form-control" placeholder="Domain" v-model="ingress.host">
                         <div class="form-control-focus">
                         </div>
                     </div>
@@ -34,15 +34,19 @@
                 </form>
                 <h4><strong>Backend Information</strong></h4>
                 <form class="form-inline margin-bottom-40" role="form">
-                    <div class="form-group form-md-line-input has-success">
-                        <input type="text" class="form-control" placeholder="Service Name" v-model="ingress.serviceName">
-                        <div class="form-control-focus">
-                        </div>
+                    <div class="form-group form-md-line-input form-md-floating-label has-info">
+                        <select class="form-control" v-model="ingress.serviceName">
+                            <option value="" disabled hidden>Select Service</option>
+                            <option :value="s.name" v-for="s in services">{{s.name}}</option>
+                        </select>
+                        <label>Select Service</label>
                     </div>
-                    <div class="form-group form-md-line-input has-success">
-                        <input type="text" class="form-control" placeholder="Service Port" v-model.number="ingress.servicePort">
-                        <div class="form-control-focus">
-                        </div>
+                    <div class="form-group form-md-line-input form-md-floating-label has-info">
+                        <select class="form-control" v-model="ingress.servicePort">
+                            <option value="" disabled hidden>Select Port</option>s
+                            <option :value="p.port" v-for="p in ports">{{p.name}}({{p.port}}/TCP)</option>
+                        </select>
+                        <label>Select Port</label>
                     </div>
                 </form>
                 <h4><strong>Advanced Settings</strong></h4>
@@ -70,6 +74,7 @@
                     servicePort: null
                 },
                 deploy_at_once: false,
+                services: []
             }
         },
         methods: {
@@ -82,6 +87,28 @@
                 }, response => {
                     // TODO: handle error
                 });
+            }
+        },
+        mounted() {
+            this.$http.get('users/' + this.$route.params.username + '/services').then(response => {
+                this.services = response.data;
+            }, response => {
+                // TODO: handle error.
+            });
+        },
+        computed: {
+            ports() {
+                console.log(this.serviceName);
+                if(this.ingress.serviceName) {
+                    for(const s of this.services) {
+                        if(s.name == this.ingress.serviceName) {
+                            return s.ports.filter(p => {
+                                return p.protocol == "TCP";
+                            });
+                        }
+                    }
+                }
+                return [];
             }
         },
         components: {
--- src/components/new-service.vue
+++ src/components/new-service.vue
@@ -1,73 +1,83 @@
 <template>
 <div class="row">
     <div class="col-md-6 col-md-offset-3">
-		<div class="portlet light bordered">
-			<div class="portlet-title">
-				<div class="caption font-green-haze">
-					<i class="icon-settings font-green-haze"></i>
-					<span class="caption-subject bold uppercase"> Create New Service </span>
-				</div>
+        <div class="portlet light bordered">
+            <div class="portlet-title">
+                <div class="caption font-green-haze">
+                    <i class="icon-settings font-green-haze"></i>
+                    <span class="caption-subject bold uppercase"> Create New Service </span>
+                </div>
                 <div class="actions">
-					<button class="btn btn-circle btn-icon-only green" @click="saveService()"><i class="fa fa-check"></i></button>
+                    <button class="btn btn-circle btn-icon-only green" @click="saveService()"><i class="fa fa-check"></i></button>
                     <button class="btn btn-circle btn-icon-only red" @click="routeToDashboard()"><i class="fa fa-times"></i></button>
-				</div>
-			</div>
-			<div class="portlet-body">
-				<h4><strong>Service Information</strong></h4>
-				<form class="form-inline margin-bottom-40" role="form">
-					<div class="form-group form-md-line-input has-success">
-						<input type="text" class="form-control" placeholder="Service Name" v-model="service.name">
-						<div class="form-control-focus">
-                        </div>
-					</div>
+                </div>
+            </div>
+            <div class="portlet-body">
+                <h4><strong>Service Information</strong></h4>
+                <form class="form-inline margin-bottom-40" role="form">
                     <div class="form-group form-md-line-input has-success">
-						<input type="text" class="form-control" placeholder="Service Description">
-						<div class="form-control-focus">
+                        <input type="text" class="form-control" placeholder="Service Name" v-model="service.name">
+                        <div class="form-control-focus">
                         </div>
-					</div>
+                    </div>
                     <div class="form-group form-md-line-input has-success">
-						<input type="text" class="form-control" placeholder="Backend" v-model="service.backend">
-						<div class="form-control-focus">
+                        <input type="text" class="form-control" placeholder="Service Description">
+                        <div class="form-control-focus">
                         </div>
-					</div>     
-				</form>
+                    </div>
+                    <div class="form-group form-md-line-input has-success">
+                        <input type="text" class="form-control" placeholder="Backend" v-model="service.backend">
+                        <div class="form-control-focus">
+                        </div>
+                    </div>
+                    <div class="form-group form-md-line-input form-md-floating-label has-info">
+                        <select class="form-control" v-model="service.backend">
+                            <option value="" disabled hidden>Select Backend</option>s
+                            <option :value="d.name" v-for="d in deployments">{{d.name}}</option>
+                        </select>
+                        <label>Select Backend</label>
+                    </div>
+                </form>
                 <h4><strong>Ports Information</strong></h4>
-				<form class="form-inline margin-bottom-40" role="form" v-for="(p, i) in service.ports">
-					<div class="form-group form-md-line-input has-success">
-						<input type="text" class="form-control" placeholder="Name" v-model="p.name">
-						<div class="form-control-focus">
-                        </div>
-					</div>
+                <form class="form-inline margin-bottom-40" role="form" v-for="(p, i) in service.ports">
                     <div class="form-group form-md-line-input has-success">
-						<input type="text" class="form-control" placeholder="Protocol" v-model="p.protocol">
-						<div class="form-control-focus">
+                        <input type="text" class="form-control" placeholder="Name" v-model="p.name">
+                        <div class="form-control-focus">
                         </div>
-					</div>
+                    </div>
+                    <div class="form-group form-md-line-input form-md-floating-label has-info">
+                        <select class="form-control" v-model="p.protocol">
+                            <option value="" disabled hidden>Select Protocol</option>s
+                            <option value="TCP">TCP</option>
+                            <option value="UDP">UDP</option>
+                        </select>
+                        <label>Select Protocol</label>
+                    </div>
                     <div class="form-group form-md-line-input has-success">
-						<input type="number" class="form-control" placeholder="Port" v-model.number="p.port">
-						<div class="form-control-focus">
+                        <input type="number" class="form-control" placeholder="Port" v-model.number="p.port">
+                        <div class="form-control-focus">
                         </div>
-					</div>
+                    </div>
                     <div class="form-group form-md-line-input has-success">
-						<input type="number" class="form-control" placeholder="Target Port" v-model.number="p.targetPort">
-						<div class="form-control-focus">
+                        <input type="number" class="form-control" placeholder="Target Port" v-model.number="p.targetPort">
+                        <div class="form-control-focus">
                         </div>
-					</div>
-					<div class="form-group form-md-line-input has-success" v-if="can_remove_port">
-						<button type="button" class="btn btn-danger" @click="removePort(i)"><i class="fa fa-minus"></i></button>
-					</div>
-				</form>
-				<form class="form-inline margin-bottom-40" role="form">
-					<div class="form-group form-md-line-input has-success">
-						<button type="button" class="btn btn-success" @click="addPort()"><i class="fa fa-plus"></i></button>
-					</div>
-				</form>
+                    </div>
+                    <div class="form-group form-md-line-input has-success" v-if="can_remove_port">
+                        <button type="button" class="btn btn-danger" @click="removePort(i)"><i class="fa fa-minus"></i></button>
+                    </div>
+                </form>
+                <form class="form-inline margin-bottom-40" role="form">
+                    <div class="form-group form-md-line-input has-success">
+                        <button type="button" class="btn btn-success" @click="addPort()"><i class="fa fa-plus"></i></button>
+                    </div>
+                </form>
                 <h4><strong>Advanced Settings</strong></h4>
                 <form class="form-horizontal margin-bottom-40" role="form">
                     <md-ckbox content="Deploy After Created" v-model="deploy_at_once"></md-ckbox>
-				</form>
-			</div>
-		</div>
+                </form>
+            </div>
+        </div>
     </div>
 </div>
 </template>
@@ -91,7 +101,15 @@
                     ]
                 },
                 deploy_at_once: false,
+                deployments: []
             }
+        },
+        mounted() {
+            this.$http.get('users/' + this.$route.params.username + '/deployments').then(response => {
+                this.deployments = response.data;
+            }, response => {
+                // TODO: handle error.
+            });
         },
         methods: {
             addPort() {
